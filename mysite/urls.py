99ae055019f39afa2516e998198170def95d06c8
김수진 <sjkim910115@gmail.com>
2020-06-08 01:44:36 +0900

    2020.06.06

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,14 +18,24 @@ from django.urls import path, include
 from mysite.views import HomeView
 from django.conf.urls.static import static
 from django.conf import settings
+
+from mysite.views import HomeView
+from mysite.views import UserCreateView, UserCreateDoneTV
 #from bookmark.views import BookmarkLV, BookmarkDV
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('accounnts/', include('django.contrib.auth.urls')),
+    path('accounts/register/', UserCreateView.as_view(), name='register'),
+    path('accounts/register/done/', UserCreateDoneTV.as_view(), name='register_done'),
+
     path('',HomeView.as_view(), name='home'),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
     path('photo/', include('photo.urls')),
+
+
 
     #path('bookmark/', BookmarkLV.as_view(), name='index'),
     #path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
