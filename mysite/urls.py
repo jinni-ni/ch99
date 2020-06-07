0fabef850362b97b3169564aec30c4131546b457
김수진 <sjkim910115@gmail.com>
2020-06-08 00:49:52 +0900

    2020.06.08

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from mysite.views import HomeView
+from django.conf.urls.static import static
+from django.conf import settings
 #from bookmark.views import BookmarkLV, BookmarkDV
 
 urlpatterns = [
@@ -23,7 +25,9 @@ urlpatterns = [
     path('',HomeView.as_view(), name='home'),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
+    path('photo/', include('photo.urls')),
 
     #path('bookmark/', BookmarkLV.as_view(), name='index'),
     #path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
